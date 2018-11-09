95536935df165a16640ff56b168bf7be12a2c957
Travis Nguyen <travisn714@gmail.com>
2018-11-08 17:07:18 -0800

    Pass the image as a prop to index.js and verify that the image is rendered. #1

--- src/app.js
+++ src/app.js
@@ -1,4 +1,20 @@
 import React from 'react'
+import { Container } from 'reactstrap'
+
+const styles = {
+  image: {
+    width: '500px',
+    height: '250px'
+  }
+}
+
+const images = [
+  {
+    src: 'http://tonsoffacts.com/wp-content/uploads/2018/01/stary-night-painting-vincent-van-gogh-starry-night-hand-painted-oil-painting-on-canvas-pictures.jpg',
+    title: 'The Starry Night',
+    caption: 'The Starry Night was painted by Vincent van Gogh in 1889.'
+  }
+]
 
 export default class App extends React.Component {
   constructor(props) {
@@ -9,9 +25,14 @@ export default class App extends React.Component {
   }
   render() {
     return(
-      <div>
-        <img></img>
-      </div>
+      <Container className="d-flex justify-content-center">
+        <div className="text-center">
+          <h2 className="mt-5">Responsive Carousel</h2>
+          <img className="mt-5" src={this.props.image} style={styles.image}></img>
+          <h4 className="mt-4">The Starry Night</h4>
+          <p className="mt-3">The Starry Night was painted by Vincent van Gogh in 1889. </p>
+        </div>
+      </Container>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app'
 
+const images = [
+  'http://tonsoffacts.com/wp-content/uploads/2018/01/stary-night-painting-vincent-van-gogh-starry-night-hand-painted-oil-painting-on-canvas-pictures.jpg'
+]
+
 ReactDOM.render(
-  <App/>,
+  <App image={images}/>,
   document.getElementById('root')
 )
