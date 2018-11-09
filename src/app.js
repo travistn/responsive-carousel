552b94608d35d3eca5df0715a77bb5ff176dd8d5
Travis Nguyen <travisn714@gmail.com>
2018-11-09 01:40:34 -0800

    Define a handleClick function that changes the image on click. #3

--- src/app.js
+++ src/app.js
@@ -14,20 +14,55 @@ export default class App extends React.Component {
     this.state = {
       currentIndex: 0
     }
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick(direction) {
+    const { image } = this.props
+    const index = this.state.currentIndex
+    switch (direction) {
+      case 'left':
+        if (index === 0) {
+          this.setState({
+            currentIndex: image.length - 1
+          })
+        }
+        else {
+          this.setState({
+            currentIndex: index - 1
+          })
+        }
+        break
+      case 'right':
+        if (index === image.length - 1) {
+          this.setState({
+            currentIndex: 0
+          })
+        }
+        else {
+          this.setState({
+            currentIndex: index + 1
+          })
+        }
+    }
   }
   render() {
+    const currentIndex = this.state.currentIndex
+    const { image } = this.props
+    const images = image.map(item => item)
     return(
       <Container className="d-flex justify-content-center">
         <div className="text-center">
           <h2 className="mt-5">Famous Paintings</h2>
           <div>
             <span>
-              <i className="far fa-2x fa-arrow-alt-circle-left mr-2"></i>
-              <img className="mt-5" src={this.props.image.map(item => item.src)} style={styles.image}></img>
-              <i className="far fa-2x fa-arrow-alt-circle-right ml-2"></i>
+              <i className="far fa-2x fa-arrow-alt-circle-left mr-2"
+                onClick={() => this.handleClick('left')}></i>
+              <img className="mt-5" src={images[currentIndex].src} style={styles.image}></img>
+              <i className="far fa-2x fa-arrow-alt-circle-right ml-2"
+                onClick={() => this.handleClick('right')}></i>
             </span>
-            <h4 className="mt-4">{this.props.image.map(item => item.title)}</h4>
-            <p className="mt-3">{this.props.image.map(item => item.caption)} </p>
+            <h4 className="mt-4">{images[currentIndex].title}</h4>
+            <p className="mt-3">{images[currentIndex].caption} </p>
           </div>
         </div>
       </Container>
--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import App from './app'
 
 const images = [
   {
-    src:'http://tonsoffacts.com/wp-content/uploads/2018/01/stary-night-painting-vincent-van-gogh-starry-night-hand-painted-oil-painting-on-canvas-pictures.jpg',
+    src: 'http://tonsoffacts.com/wp-content/uploads/2018/01/stary-night-painting-vincent-van-gogh-starry-night-hand-painted-oil-painting-on-canvas-pictures.jpg',
     title: 'The Starry Night',
     caption: 'The Starry Night was painted by Vincent van Gogh in 1889.'
+  },
+  {
+    src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/%27Adam%27s_Creation_Sistine_Chapel_ceiling%27_by_Michelangelo_JBU33cut.jpg/600px-%27Adam%27s_Creation_Sistine_Chapel_ceiling%27_by_Michelangelo_JBU33cut.jpg',
+    title: 'The Creation of Adam',
+    caption: 'The Creation of Adam was painted by Michelangelo between 1508 - 1512.'
   }
 ]
-
-const image = images.map(item => item)
 
 ReactDOM.render(
   <App image={images}/>,
