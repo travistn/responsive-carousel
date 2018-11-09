665abf09717e5256e192b57498934c85f3fbd54b
Travis Nguyen <travisn714@gmail.com>
2018-11-09 01:17:01 -0800

    Create arrow icons that go beside the image. #3

--- src/app.js
+++ src/app.js
@@ -3,18 +3,10 @@ import { Container } from 'reactstrap'
 
 const styles = {
   image: {
-    width: '500px',
-    height: '250px'
+    width: '250px',
+    height: '150px'
   }
 }
-
-const images = [
-  {
-    src: 'http://tonsoffacts.com/wp-content/uploads/2018/01/stary-night-painting-vincent-van-gogh-starry-night-hand-painted-oil-painting-on-canvas-pictures.jpg',
-    title: 'The Starry Night',
-    caption: 'The Starry Night was painted by Vincent van Gogh in 1889.'
-  }
-]
 
 export default class App extends React.Component {
   constructor(props) {
@@ -27,10 +19,16 @@ export default class App extends React.Component {
     return(
       <Container className="d-flex justify-content-center">
         <div className="text-center">
-          <h2 className="mt-5">Responsive Carousel</h2>
-          <img className="mt-5" src={this.props.image} style={styles.image}></img>
-          <h4 className="mt-4">The Starry Night</h4>
-          <p className="mt-3">The Starry Night was painted by Vincent van Gogh in 1889. </p>
+          <h2 className="mt-5">Famous Paintings</h2>
+          <div>
+            <span>
+              <i className="far fa-2x fa-arrow-alt-circle-left mr-2"></i>
+              <img className="mt-5" src={this.props.image.map(item => item.src)} style={styles.image}></img>
+              <i className="far fa-2x fa-arrow-alt-circle-right ml-2"></i>
+            </span>
+            <h4 className="mt-4">{this.props.image.map(item => item.title)}</h4>
+            <p className="mt-3">{this.props.image.map(item => item.caption)} </p>
+          </div>
         </div>
       </Container>
     )
--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom'
 import App from './app'
 
 const images = [
-  'http://tonsoffacts.com/wp-content/uploads/2018/01/stary-night-painting-vincent-van-gogh-starry-night-hand-painted-oil-painting-on-canvas-pictures.jpg'
+  {
+    src:'http://tonsoffacts.com/wp-content/uploads/2018/01/stary-night-painting-vincent-van-gogh-starry-night-hand-painted-oil-painting-on-canvas-pictures.jpg',
+    title: 'The Starry Night',
+    caption: 'The Starry Night was painted by Vincent van Gogh in 1889.'
+  }
 ]
+
+const image = images.map(item => item)
 
 ReactDOM.render(
   <App image={images}/>,
